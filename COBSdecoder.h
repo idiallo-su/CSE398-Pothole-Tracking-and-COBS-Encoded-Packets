d94620c6cdfdf1fd80ef51ef5e94c0f0143872bc
idiallo-su <113380366+idiallo-su@users.noreply.github.com>
2023-04-19 13:01:46 -0400

    Create COBSdecoder.h

---
+++ COBSdecoder.h
@@ -0,0 +1,20 @@
+// The COBS decoder class header file
+
+#include <iostream>
+
+class cobSerial {
+	public:
+		cobSerial();
+		~cobSerial();
+		void addByte(uint8_t c);
+		bool packetAvail();
+		int getPacket(char* ptrOutput);
+		static const int constMaxPacketSize = 256;
+	private:
+		void decode();
+		bool bAvail;
+		char buff[constMaxPacketSize];
+		int buffIdx;
+		char decodedBuff[constMaxPacketSize];
+		int decodeIdx;
+};
